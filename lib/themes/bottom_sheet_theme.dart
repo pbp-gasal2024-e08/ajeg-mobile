cca91f87c4d94fe043d638ef31a0adfbcf7d94ba
MNTYROS <mnadzimtahara@gmail.com>
2024-12-02 15:48:57 +0700

    first theme

---
+++ lib/themes/bottom_sheet_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TBottomSheetTheme {
+  TBottomSheetTheme._();
+
+  static BottomSheetThemeData lightBottomSheetTheme = const BottomSheetThemeData(
+    backgroundColor: Colors.white,
+  );
+}
