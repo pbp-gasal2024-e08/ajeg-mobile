1a9238d1ddbdd9adb1eb9ecd31df2ee1f5a6a11e
anandajoyyy22 <ananda.joy@ui.ac.id>
2024-12-21 13:05:26 +0700

    Voucher done

--- lib/models/voucher.dart
+++ lib/models/voucher.dart
@@ -1,3 +1,68 @@
+// // To parse this JSON data, do
+// //
+// //     final voucher = voucherFromJson(jsonString);
+
+// import 'dart:convert';
+
+// List<Voucher> voucherFromJson(String str) => List<Voucher>.from(json.decode(str).map((x) => Voucher.fromJson(x)));
+
+// String voucherToJson(List<Voucher> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+// class Voucher {
+//     String model;
+//     int pk;
+//     Fields fields;
+
+//     Voucher({
+//         required this.model,
+//         required this.pk,
+//         required this.fields,
+//     });
+
+//     factory Voucher.fromJson(Map<String, dynamic> json) => Voucher(
+//         model: json["model"],
+//         pk: json["pk"],
+//         fields: Fields.fromJson(json["fields"]),
+//     );
+
+//     Map<String, dynamic> toJson() => {
+//         "model": model,
+//         "pk": pk,
+//         "fields": fields.toJson(),
+//     };
+// }
+
+// class Fields {
+//     String code;
+//     String discount;
+//     DateTime expiryDate;
+//     bool isClaimed;
+//     bool isFlashSale;
+
+//     Fields({
+//         required this.code,
+//         required this.discount,
+//         required this.expiryDate,
+//         required this.isClaimed,
+//         required this.isFlashSale,
+//     });
+
+//     factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+//         code: json["code"],
+//         discount: json["discount"],
+//         expiryDate: DateTime.parse(json["expiry_date"]),
+//         isClaimed: json["is_claimed"],
+//         isFlashSale: json["is_flash_sale"],
+//     );
+
+//     Map<String, dynamic> toJson() => {
+//         "code": code,
+//         "discount": discount,
+//         "expiry_date": expiryDate.toIso8601String(),
+//         "is_claimed": isClaimed,
+//         "is_flash_sale": isFlashSale,
+//     };
+// }
 // To parse this JSON data, do
 //
 //     final voucher = voucherFromJson(jsonString);
@@ -9,7 +74,7 @@ List<Voucher> voucherFromJson(String str) => List<Voucher>.from(json.decode(str)
 String voucherToJson(List<Voucher> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Voucher {
-    String model;
+    Model model;
     int pk;
     Fields fields;
 
@@ -20,13 +85,13 @@ class Voucher {
     });
 
     factory Voucher.fromJson(Map<String, dynamic> json) => Voucher(
-        model: json["model"],
+        model: modelValues.map[json["model"]]!,
         pk: json["pk"],
         fields: Fields.fromJson(json["fields"]),
     );
 
     Map<String, dynamic> toJson() => {
-        "model": model,
+        "model": modelValues.reverse[model],
         "pk": pk,
         "fields": fields.toJson(),
     };
@@ -62,4 +127,24 @@ class Fields {
         "is_claimed": isClaimed,
         "is_flash_sale": isFlashSale,
     };
+}
+
+enum Model {
+    VOUCHERS_VOUCHER
+}
+
+final modelValues = EnumValues({
+    "vouchers.voucher": Model.VOUCHERS_VOUCHER
+});
+
+class EnumValues<T> {
+    Map<String, T> map;
+    late Map<T, String> reverseMap;
+
+    EnumValues(this.map);
+
+    Map<T, String> get reverse {
+            reverseMap = map.map((k, v) => MapEntry(v, k));
+            return reverseMap;
+    }
 }
--- lib/screens/list_voucher.dart
+++ lib/screens/list_voucher.dart
@@ -4,6 +4,149 @@ import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import '../widgets/left_drawer.dart';
 
+class VoucherCard extends StatelessWidget {
+  final String code;
+  final String discount;
+  final String expiryDate;
+  final bool isClaimed;
+
+  const VoucherCard({
+    Key? key,
+    required this.code,
+    required this.discount,
+    required this.expiryDate,
+    required this.isClaimed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        padding: const EdgeInsets.all(16.0),
+        width: MediaQuery.of(context).size.width * 0.9, // 90% of screen width
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 6,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Discount section
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+              decoration: BoxDecoration(
+                color: Colors.orange.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(4.0),
+              ),
+              child: Text(
+                discount,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 12),
+            // Voucher details
+            Text(
+              "Code: $code",
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              "Expiry Date: $expiryDate",
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 12),
+            // Status section
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Icon(
+                  Icons.verified,
+                  color: Colors.orange,
+                  size: 16,
+                ),
+                const SizedBox(width: 4),
+                Text(
+                  isClaimed ? 'Voucher verified' : 'Voucher not claimed',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: isClaimed ? Colors.orange : Colors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            // Claim button at the bottom
+            Align(
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                onPressed: isClaimed
+                    ? null
+                    : () async {
+                        final response = await request.post(
+                          "http://127.0.0.1:8000/vouchers/claim-voucher/$code/",
+                          {},
+                        );
+
+                        if (response['status'] == 'success') {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Voucher $code claimed successfully!'),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Failed to claim voucher: ${response['message']}'),
+                            ),
+                          );
+                        }
+                      },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  disabledBackgroundColor: Colors.grey[300],
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                ),
+                child: Text(
+                  isClaimed ? 'Claimed' : 'Claim Voucher',
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class VoucherPage extends StatefulWidget {
   const VoucherPage({super.key});
 
@@ -14,7 +157,8 @@ class VoucherPage extends StatefulWidget {
 class _VoucherPageState extends State<VoucherPage> {
   Future<List<Voucher>> fetchVoucher(CookieRequest request) async {
     // TOD Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-    final response = await request.get('http://127.0.0.1:8000/json/');
+    final response =
+        await request.get('http://127.0.0.1:8000/vouchers/vouchers-json/');
 
     // Melakukan decode response menjadi bentuk json
     var data = response;
@@ -46,7 +190,10 @@ class _VoucherPageState extends State<VoucherPage> {
             return const Center(
               child: Text(
                 'No vouchers available.',
-                style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 236, 155, 88)),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromARGB(255, 236, 155, 88),
+                ),
               ),
             );
           } else {
@@ -54,30 +201,12 @@ class _VoucherPageState extends State<VoucherPage> {
               itemCount: snapshot.data!.length,
               itemBuilder: (_, index) {
                 final voucher = snapshot.data![index].fields;
-                return Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Code: ${voucher.code}",
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text("Discount: ${voucher.discount}"),
-                      const SizedBox(height: 10),
-                      Text("Expiry Date: ${voucher.expiryDate.toLocal().toString().split(' ')[0]}"),
-                      const SizedBox(height: 10),
-                      Text("Claimed: ${voucher.isClaimed ? 'Yes' : 'No'}"),
-                      const SizedBox(height: 10),
-                      Text("Flash Sale: ${voucher.isFlashSale ? 'Yes' : 'No'}"),
-                    ],
-                  ),
+                return VoucherCard(
+                  code: voucher.code,
+                  discount: voucher.discount,
+                  expiryDate:
+                      voucher.expiryDate.toLocal().toString().split(' ')[0],
+                  isClaimed: voucher.isClaimed,
                 );
               },
             );
